b7e4a1b17f720e4b507343987e1c68d03c06dcac
ViniJordao <viniciusjordao02@gmail.com>
2021-05-10 15:27:18 -0300

    Adicionar arquivos de projeto.

---
+++ Triangulo.ConsoleApp/Controlador.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Triangulo.ConsoleApp
+{
+    public class Controlador 
+    {
+        public int x;
+        public int y;
+        public int z;
+        public void CalcularTriangulos()
+        {
+            if (x == y && y == z)
+            {
+                Console.WriteLine("Triangulo Eqüilátero");
+            }
+            else if (x == y || y == z || x == z)
+            {
+                Console.WriteLine("Triangulo Isósceles");
+            }
+            else if (x != y || y != z || x != z)
+            {
+                Console.WriteLine("Triangulo Escaleno");
+            }
+            else
+            {
+                Console.WriteLine("Triangulo Inválido");
+            }
+        }
+    }
+}
+
+
---
+++ Triangulo.ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Triangulo.ConsoleApp
+{
+    public class Program
+    {
+        static Controlador controlador = new Controlador();
+        static void Main(string[] args)
+        {
+            controlador.x = 1;
+            controlador.y = 2;
+            controlador.z = 3;
+            controlador.CalcularTriangulos();
+        }
+    }
+}
